23dd7e54ea8e150c774fc03a55ae1de67464ac5f
kgs233 <kgs233@126.com>
2022-07-22 20:23:25 +0800

    patch

--- dwm/config.h
+++ dwm/config.h
@@ -5,6 +5,7 @@ static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
+static const int user_bh            = 3;        /* 2 is the default spacing around the bar's font */
 static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int gappo     = 6;
 static const int vertpad            = 6;       /* vertical padding of bar */
